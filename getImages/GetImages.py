0e2f2b93fb27ed58ac0708460753180959b03e9e
Ismail Wahdan <ismailwahdan@uni-bonn.de>
2020-02-03 10:13:28 +0100

    added script to get images

---
+++ getImages/GetImages.py
@@ -0,0 +1,27 @@
+import json
+import os
+
+
+import json
+mode = input("val/test?")
+file_source = ""
+if (mode=="val"):
+	file_source = '../data/nocaps/annotations/nocaps_val_image_info.json'
+else:
+	file_source = '../data/nocaps/annotations/nocaps_test_image_info.json'
+
+url=""
+ID =""
+with open(file_source) as json_file:
+	data = json.load(json_file)
+	for image in data['images']:
+		url = image['coco_url']
+		ID = image['id']
+		command = 'wget -O '+mode+'/'+str(ID)+'.jpg '+url
+		print(command)
+		os.system(command)
+
+
+
+
+
