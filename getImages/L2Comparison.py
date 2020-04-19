495aaa153862140cca3d268791c950283f02d51e
Ismail Wahdan <esmaeel.wahdan@gmail.com>
2020-04-19 16:02:18 +0200

    transport L2Comparison file

--- getImages/L2Comparison.py
+++ getImages/L2Comparison.py
@@ -7,16 +7,34 @@ Created on Sun Apr 19 15:15:29 2020
 
 """
 Steps:
-    1- find 2 boxes with same class id 
-    2- find their features
-    3- compare their L2 distance
+    1- get class_label
+    2- look for boxes with this class label
+    3- get image_id of this box
+    4- open this image, look for this box, get its feature
+    5- calculated L2 distance, add to list of distances
 """
 
 
 import json
+import h5py
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from PIL import Image
 
+filename = 'nocaps_val_vg_detector_features_adaptive.h5'    
 
+class_label = int(input("enter class id to find average distance"))
 
-with open('nocaps_val_detections.json','r') as oi_result:
+with open('nocaps_val_detections.json','r') as oi_result, h5py.File(filename, 'r') as vg_result:
     results = json.load(oi_result)
-    print(results['categories'][0])
+    distances_same_label = []
+    for a in results['annotations']:
+        if a['category_id'] == class_label:
+            look_for_box = a['bbox']
+            in_image = a['image_id']
+            boxes = np.array(vg_result['boxes'][in_image])
+            boxes = boxes.reshape(boxes.size//4,4)
+            print(look_for_box)
+            print(boxes)
