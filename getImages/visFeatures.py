97585551c6f7189b106cd8f6ed69c500076bd056
Ismail Wahdan <ismailwahdan@uni-bonn.de>
2020-04-19 15:05:09 +0200

    push visFeatures.py

--- getImages/visFeatures.py
+++ getImages/visFeatures.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from PIL import Image
 
-filename = '../data/nocaps_val_vg_detector_features_adaptive.h5'
+filename = '../data/coco_val2017_vg_detector_features_adaptive.h5'
 ID = int(input("enter image id to visualize"))
 image = cv2.imread('im'+str(ID)+'.jpg')
 
@@ -24,15 +24,20 @@ with h5py.File(filename, 'r') as f:
     # Get the data
     data = list(f[a_group_key])
     index = 0
-    for i in range(4500):
-        if (f['image_id'][i]==ID):
-            index = i
-            break
-    print(f['features'][index])
-    
+    # for i in range(4500):
+    #     if (f['image_id'][i]==ID):
+    #         index = i
+            # break
+    print (f['image_id'][index])
     boxes = np.array(f['boxes'][index])
     boxes = boxes.reshape(boxes.size//4,4)
-    print(boxes)
+    feat = np.array(f['features'][index])
+    feat = feat.reshape(feat.size//2048,2048)
+    print(boxes.shape)
+    print (f['height'][index],f['width'][index],np.amax(boxes,axis=0))
+    # print(feat.shape, boxes.shape)
+    raise Exception()
+    # print(boxes)
     fig,ax = plt.subplots(1)
     ax.imshow(image)
     
@@ -40,7 +45,7 @@ with h5py.File(filename, 'r') as f:
     colors = [cmap(i) for i in np.linspace(0, 1, boxes.shape[0])]
     
     for i,box in enumerate(boxes):
-    	if f['features'][index][i]>=0.5:
+    	#if f['features'][index][i]>=0.5:
 	        # Create a Rectangle patch
 	        rect = patches.Rectangle((box[0],box[1]),box[2]-box[0],box[3]-box[1],linewidth=1,edgecolor=colors[i],facecolor='none')
 	        # Add the patch to the Axes
