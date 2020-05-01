5162e4fbb5bf332676c815f9dc297ed48e120494
Ismail Wahdan <esmaeel.wahdan@gmail.com>
2020-05-01 13:33:15 +0200

    transfer

--- updown/utils/GraphBuilder.py
+++ updown/utils/GraphBuilder.py
@@ -10,8 +10,10 @@ from scipy.linalg import block_diag
 
 
 def calc_distance(box1, box2):
+    print("here fine 11")
     mid_point_1 = torch.Tensor([ box1[2] - box1[0] , box1[3]- box1[1]])
     mid_point_2 = torch.Tensor([ box2[2] - box2[0] , box2[3] - box2[1]])
+    print("here fine 12")
     return  np.linalg.norm (mid_point_1 - mid_point_2)
 
 
@@ -37,10 +39,11 @@ def build_batch_graph(batch_features:  torch.FloatTensor, batch_boxes:  torch.Fl
         boxes = batch_boxes[idx]
         A = get_adj_mat(boxes)
         adj_matrices.append(A)
-    
+    print("Here find 13")
     batch_adj_Matrix = block_diag(*adj_matrices)
     #batch_features = batch_features.cpu()
     batch_feature_Matrix = block_diag(*batch_features)
+    print("here fine 14")
     
     return torch.FloatTensor(batch_adj_Matrix), torch.FloatTensor(batch_feature_Matrix)
     
