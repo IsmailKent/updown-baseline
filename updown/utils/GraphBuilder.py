c8759e1ecd7be5001815573dc46633a305ffda6b
Ismail Wahdan <esmaeel.wahdan@gmail.com>
2020-05-03 13:34:28 +0200

    transfer

--- updown/utils/GraphBuilder.py
+++ updown/utils/GraphBuilder.py
@@ -17,7 +17,7 @@ Memory allocation: Whenever you know the size of an object, preallocate space fo
 def calc_distance(box1, box2):
     mid_point_1 = torch.Tensor([box1[0],box1[1]]) + torch.Tensor([ box1[2] - box1[0] , box1[3]- box1[1]])
     mid_point_2 = torch.Tensor([box2[0],box2[1]]) + torch.Tensor([ box2[2] - box2[0] , box2[3] - box2[1]])
-    return  np.linalg.norm (mid_point_1 - mid_point_2)
+    return  np.linalg.norm (mid_point_1 - mid_point_2) + 0
 
 
 def get_adj_mat(image_boxes:  torch.FloatTensor):
