abb272c501ecdde62b65a3c93c03b2d4ef25a29c
Ismail Wahdan <esmaeel.wahdan@gmail.com>
2020-05-03 14:40:22 +0200

    transfer

--- updown/utils/GraphBuilder.py
+++ updown/utils/GraphBuilder.py
@@ -54,7 +54,7 @@ def build_batch_graph(batch_features:  torch.FloatTensor, batch_boxes:  torch.Fl
     print("here ok 3")
     
     
-    return batch_adj_Matrix, batch_feature_Matrix
+    return torch.from_numpy(batch_adj_Matrix), batch_feature_Matrix
     
  
             
