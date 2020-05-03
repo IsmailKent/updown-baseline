4883dd2fe30f95c3792ec9462aa2449dedfa9ede
Ismail Wahdan <esmaeel.wahdan@gmail.com>
2020-05-03 15:09:21 +0200

    transfer

--- updown/utils/GraphBuilder.py
+++ updown/utils/GraphBuilder.py
@@ -7,6 +7,7 @@ Created on Sat Apr 25 11:13:58 2020
 import torch 
 import numpy as np
 from scipy.linalg import block_diag
+import math
 
 """
 Loops: Whenever you think you need a loop stop and think. Most of the time you do not and in fact do not even want one. It is much faster both to write and run code without loops.
@@ -17,7 +18,7 @@ Memory allocation: Whenever you know the size of an object, preallocate space fo
 def calc_distance(box1, box2):
     mid_point_1 = torch.Tensor([box1[0],box1[1]]) + torch.Tensor([ box1[2] - box1[0] , box1[3]- box1[1]])
     mid_point_2 = torch.Tensor([box2[0],box2[1]]) + torch.Tensor([ box2[2] - box2[0] , box2[3] - box2[1]])
-    return  np.linalg.norm (mid_point_1 - mid_point_2)
+    return  math.sqrt( (mid_point_1[0]-mid_point_2[0])**2 + (mid_point_1[1]-mid_point_2[1])**2)
 
 
 def get_adj_mat(image_boxes:  torch.FloatTensor):
@@ -29,11 +30,10 @@ def get_adj_mat(image_boxes:  torch.FloatTensor):
             if idx1==idx2:
                 continue
             box2 = image_boxes[idx2]
-            dist = calc_distance(box1,box2) + 0
+            dist = calc_distance(box1,box2)
             A[idx1][idx2] = dist
             A[idx2][idx1]= dist
-    A = torch.exp(-A/500)   
-
+    A = torch.exp(-A/500)    
     return torch.FloatTensor(A)
           
             
@@ -42,6 +42,7 @@ def build_batch_graph(batch_features:  torch.FloatTensor, batch_boxes:  torch.Fl
     for idx in range(batch_boxes.shape[0]):
         boxes = batch_boxes[idx]
         A = get_adj_mat(boxes)
+        print(A.shape)
         adj_matrices[idx] = A.squeeze()
     batch_adj_Matrix = block_diag(*adj_matrices)
     batch_features = list(batch_features)
@@ -49,7 +50,7 @@ def build_batch_graph(batch_features:  torch.FloatTensor, batch_boxes:  torch.Fl
     batch_feature_Matrix = torch.stack(list(batch_features))
     
     
-    return torch.from_numpy(batch_adj_Matrix), batch_feature_Matrix
+    return torch.FloatTensor(batch_adj_Matrix), torch.FloatTensor(batch_feature_Matrix)
     
  
             
