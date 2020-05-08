9109141b5b0e798f58a7cc6033b5a3a44c5065b8
Ismail Wahdan <ismailwahdan@uni-bonn.de>
2020-05-08 17:40:47 +0200

    transfer

--- scripts/train.py
+++ scripts/train.py
@@ -209,7 +209,7 @@ if __name__ == "__main__":
         print("batch loss: ",batch_loss)
 
         batch_loss.backward()
-        print(model.parameter())
+        print(model.parameters())
         nn.utils.clip_grad_norm_(model.parameters(), _C.OPTIM.CLIP_GRADIENTS)
 
         optimizer.step()
