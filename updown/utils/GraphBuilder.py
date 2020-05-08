ce30a3ced650314174eca0175f3123215bd1964f
Ismail Wahdan <esmaeel.wahdan@gmail.com>
2020-05-08 19:24:21 +0200

    transfer

--- updown/utils/GraphBuilder.py
+++ updown/utils/GraphBuilder.py
@@ -27,7 +27,7 @@ def get_adj_mat(image_boxes:  torch.FloatTensor):
     y2 = torch.square(center).sum(-1).view(1,N).repeat(N,1) # shape is (N,N)
     xy = torch.mm(center,center.t()) # shape is (N,N)
     dists = torch.sqrt(x2 + y2 - 2*xy) # shape is (N, N)
-    A = torch.exp(-dists/500)    
+    A = torch.exp(-dists)    
     return A
           
             
