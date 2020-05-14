3095147055bb0ae69a9883ad0be06dcf02b6b811
Ismail Wahdan <esmaeel.wahdan@gmail.com>
2020-05-14 17:55:51 +0200

    transfer

--- getImages/getDomain.py
+++ getImages/getDomain.py
@@ -10,15 +10,29 @@ import json
 ID = int(input(("enter image id: ")))
 
 file_source = "nocaps_val_detections.json"
-
 classes = []
-with open(file_source) as json_file:
+with open(file_source) as json_file, open('domain.txt','r') as domain:
+    data = json.load(json_file)
+    domain_list = domain.readlines()
+    ids = []
+    for cat in data['categories']:
+        if cat['id'] in domain_list:
+            ids.append(cat['id'])
+    print(len(domain_list) == len(ids))
+    d = {}
+    for i in range(5000):
+        d[i] = []
     data = json.load(json_file)
     for ann in data['annotations']:
+        d[ann['image_id']].append(ann['category_id'])
         if (ann['image_id']==ID):
             classes.append(ann['category_id'])
     print(classes)
     for cat in data['categories']:
         if cat['id'] in classes:
             print(cat['name'])
+    
+    for key, value in d.items():
+        if (all(item in value for item in ids)):
+            print(key)
             
